4adf79ca15d925b7ebbbb3c6e0d47633b919173c
mbloom23 <matt.bloom101@gmail.com>
2023-03-18 17:55:26 -0400

    added comments

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
+# NOTE: MUST ADD YOUR API KEY AS AN ENV VARIABLE
 openai.api_key = os.getenv("OPENAI_API_KEY")
 sys_msg = "You are MBEL, an assistant that generates children\'s stories based on the user\'s input prompt. Stories should be less than 500 words and use simple vocabulary."
 model = "gpt-3.5-turbo"
